7883f219ae302b2cfff13a2c5b1fe7738463c23d
bjornxzz <andrealmeidaaz105@gmail.com>
2025-07-22 17:29:31 -0300

    creat first migrations

---
+++ backend/prisma/migrations/20250722202342_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "TipoUsuario" AS ENUM ('CLIENTE', 'DONO', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+    "tipo" "TipoUsuario" NOT NULL,
+    "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
