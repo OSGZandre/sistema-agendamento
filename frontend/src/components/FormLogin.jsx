5658c3f500fe6faa12f89ebc993b7f00f9246937
augus <augustoandrade36@gmail.com>
2025-07-23 16:14:01 -0300

    css atualizado login e register

--- frontend/src/components/FormLogin.jsx
+++ frontend/src/components/FormLogin.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Eye } from "lucide-react";
+import { EyeOff } from "lucide-react";
 
 export default function FormLogin({ onLogin }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
     onLogin({ email, senha });
@@ -14,31 +20,44 @@ export default function FormLogin({ onLogin }) {
   return (
     <form
       onSubmit={handleSubmit}
-      className="max-w-sm mx-auto p-6 bg-white shadow rounded"
+      className="max-w-sm mx-auto p-10 border-2 border-[#3E57B3] bg-white shadow rounded-xl min-h-96"
     >
-      <h2 className="text-xl font-bold mb-4 text-center">Login</h2>
+      <h2 className="text-xl text-[#32458C] font-bold mb-4 text-center">Login</h2>
 
+      <label className="block text-[#32458C] mt-10" >Email</label>
       <input
         type="email"
-        placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        className="w-full p-2 mb-4 border rounded"
+        className="w-full p-1 border-b-2 border-[#3E57B3] bg-white text-gray-800 placeholder-gray-600 focus:outline-none focus:border-[#8193D6]"
         required
       />
 
-      <input
-        type="password"
-        placeholder="Senha"
-        value={senha}
-        onChange={(e) => setSenha(e.target.value)}
-        className="w-full p-2 mb-4 border rounded"
-        required
-      />
+      <label className="block text-[#32458C] mt-7">Senha</label>
+      <div className="relative w-full">
+        <input
+            type={showPassword ? "text" : "password"}
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            className="w-full p-1 border-b-2 border-[#3E57B3] bg-white text-gray-800 placeholder-gray-600 focus:outline-none focus:border-[#8193D6]"
+            required
+        />
+        <button
+            type="button"
+            className="absolute right-0 top-1/2 transform -translate-y-1/2 text-[#3E57B3] hover:text-[#A7B4E3]"
+            onClick={togglePasswordVisibility}
+        >
+          {showPassword ? (
+              <EyeOff className="w-5 h-5" />
+          ) : (
+              <Eye className="w-5 h-5" />
+          )}
+        </button>
+      </div>
 
       <button
         type="submit"
-        className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
+        className="mt-10 w-full border-2 border-[#3E57B3] bg-transparent text-[#32458C] p-2 rounded-xl hover:bg-[#A7B4E3]"
       >
         Entrar
       </button>
@@ -46,7 +65,7 @@ export default function FormLogin({ onLogin }) {
       <button
         type="button"
         onClick={() => navigate("/register")}
-        className="w-full bg-gray-600 text-white p-2 rounded hover:bg-gray-700 mt-2"
+        className="mt-4 w-full border-2 border-[#3E57B3] bg-transparent text-[#32458C] p-2 rounded-xl hover:bg-[#A7B4E3] mt-2"
       >
         Não tem conta? Registre-se
       </button>
--- frontend/src/components/FormRegister.jsx
+++ frontend/src/components/FormRegister.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {Eye, EyeOff} from "lucide-react";
 
 export default function FormRegister({ onRegister }) {
   const [nome, setNome] = useState("");
@@ -7,6 +8,11 @@ export default function FormRegister({ onRegister }) {
   const [senha, setSenha] = useState("");
   const [tipo, setTipo] = useState("CLIENTE");
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,41 +22,55 @@ export default function FormRegister({ onRegister }) {
   return (
     <form
       onSubmit={handleSubmit}
-      className="max-w-sm mx-auto p-6 bg-white shadow rounded"
+      className="max-w-sm mx-auto p-8 border-2 border-[#3E57B3] bg-white shadow rounded-xl min-h-96"
     >
-      <h2 className="text-xl font-bold mb-4 text-center">Registrar</h2>
+      <h2 className="text-xl font-bold mb-4 text-center text-[#32458C]">Registrar</h2>
 
+      <label className="block text-[#32458C] mt-10" >Nome</label>
       <input
         type="text"
-        placeholder="Nome"
         value={nome}
         onChange={(e) => setNome(e.target.value)}
-        className="w-full p-2 mb-4 border rounded"
+        className="w-full p-1 border-b-2 border-[#3E57B3] bg-white text-gray-800 placeholder-gray-600 focus:outline-none focus:border-[#8193D6]"
         required
       />
 
+      <label className="block text-[#32458C] mt-7">Email</label>
       <input
         type="email"
-        placeholder="Email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        className="w-full p-2 mb-4 border rounded"
+        className="w-full p-1 border-b-2 border-[#3E57B3] bg-white text-gray-800 placeholder-gray-600 focus:outline-none focus:border-[#8193D6]"
         required
       />
 
-      <input
-        type="password"
-        placeholder="Senha"
-        value={senha}
-        onChange={(e) => setSenha(e.target.value)}
-        className="w-full p-2 mb-4 border rounded"
-        required
-      />
+      <label className="block text-[#32458C] mt-7">Senha</label>
+      <div className="relative w-full">
+        <input
+            type={showPassword ? "text" : "password"}
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+            className="w-full p-1 border-b-2 border-[#3E57B3] bg-white text-gray-800 placeholder-gray-600 focus:outline-none focus:border-[#8193D6]"
+            required
+        />
+        <button
+            type="button"
+            className="absolute right-0 top-1/2 transform -translate-y-1/2 text-[#3E57B3] hover:text-[#A7B4E3]"
+            onClick={togglePasswordVisibility}
+        >
+          {showPassword ? (
+              <EyeOff className="w-5 h-5" />
+          ) : (
+              <Eye className="w-5 h-5" />
+          )}
+        </button>
+      </div>
+
 
       <select
         value={tipo}
         onChange={(e) => setTipo(e.target.value)}
-        className="w-full p-2 mb-4 border rounded"
+        className="w-full p-2 mb-5 mt-6 border-2 text-[#32458C] border-[#3E57B3] rounded-xl"
         required
       >
         <option value="CLIENTE">Cliente</option>
@@ -60,7 +80,7 @@ export default function FormRegister({ onRegister }) {
 
       <button
         type="submit"
-        className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
+        className="w-full border-2 border-[#3E57B3] bg-transparent text-[#32458C] p-2 rounded-xl hover:bg-[#A7B4E3]"
       >
         Registrar
       </button>
@@ -68,7 +88,7 @@ export default function FormRegister({ onRegister }) {
       <button
         type="button"
         onClick={() => navigate("/login")}
-        className="w-full bg-gray-600 text-white p-2 rounded hover:bg-gray-700 mt-2"
+        className="mt-3 w-full border-2 border-[#3E57B3] bg-transparent text-[#32458C] p-2 rounded-xl hover:bg-[#A7B4E3]"
       >
         Voltar para Login
       </button>
