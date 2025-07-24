50e93b5477aa60a097ef9f6720ed18a734ffa8b1
augus <augustoandrade36@gmail.com>
2025-07-24 16:40:20 -0300

    css atualizado dono e servicos p2

--- frontend/src/pages/calendario.jsx
+++ frontend/src/pages/calendario.jsx
@@ -128,17 +128,19 @@ export default function Calendario() {
 
   return (
     <div className="min-h-screen bg-gray-100">
-      <header className="bg-blue-600 text-white p-4">
-        <div className="max-w-7xl mx-auto flex justify-between items-center">
+      <header className="bg-transparent text-black border-b-2 border-[#3E57B3] p-4">
+        <div className="max-w-5xl mx-auto flex justify-between items-center">
           <h1 className="text-2xl font-bold">
-            Calendário de Agendamentos - {usuario.nome}
+            Gerenciar Serviços - {usuario.nome}
           </h1>
-          <button
-            onClick={handleLogout}
-            className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
-          >
-            Sair
-          </button>
+          <div className="flex gap-2">
+            <button
+                onClick={handleLogout}
+                className="bg-transparent text-black px-4 py-2 rounded-xl border-2 border-[#3E57B3] hover:bg-[#A7B4E3]"
+            >
+              Voltar
+            </button>
+          </div>
         </div>
       </header>
 
